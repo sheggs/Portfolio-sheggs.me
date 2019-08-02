80749f93634e502f26b79bf82caae41715e94065
Sheggs <mahdi_moh@outlook.com>
2019-08-03 00:06:24 +0100

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+
+    <Navigation v-bind:tabs="tabs"/>
+  </div>
+</template>
+
+<script>
+import Navigation from "./components/Navigation";
+export default {
+  name: "App",
+  components: {
+    Navigation
+  },
+  data() {
+    return {
+      tabs: [
+        {
+          name: 'Home',
+          href: '#'
+        },
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Navigation.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="navbar">
+    <h1>Sheggs.me</h1>
+    <div class = "navitem" v-bind:key="tab.id" v-for="tab in tabs">
+      <a>{{tab.name}}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+props: ["tabs"],
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    *{
+        margin:0;
+        padding:0;
+    }
+    .navbar{
+        width:100vw;
+        float:left;
+        margin:0;
+        padding:0;
+    }
+    h1{
+        float: left;
+    }
+    .navitem{
+        position: relative;
+    }
+</style>
