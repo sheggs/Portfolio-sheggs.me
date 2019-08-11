30c1d0e6ca64057afc0038b921488273557ef0f6
Sheggs <mahdi_moh@outlook.com>
2019-08-11 22:50:38 +0100

    Added skills section to client side

--- client/src/components/subcomponents/AboutMe.vue
+++ client/src/components/subcomponents/AboutMe.vue
@@ -1,33 +1,65 @@
 <template>
   <div class="app">
-      <div class="aboutme_wrapper">
-          <div class="information">
-            <div class="imagecontainer">
-              <img  alt="" class="imageplaceholder">
-            </div>a
-            <h1>{{name}} </h1>
-            <h2>{{job_role}}</h2>
-            <div v-bind:key="icon.id" v-for="icon in icons"><i :class="icon.icon"> </i>{{icon.title}} : {{icon.Content}}</div>
-          </div>
-          <div class="imageofme">
-          </div>
+    <div class="aboutme_wrapper">
+      <div class="information">
+        <div class="imagecontainer">
+          <img  alt class="imageplaceholder" />
+        </div>
+        <h1>{{name}}</h1>
+        <h2>{{job_role}}</h2>
+        <div v-bind:key="icon.id" v-for="icon in icons">
+          <i :class="icon.icon"></i>
+          {{icon.title}} : {{icon.Content}}
+        </div>
       </div>
+      <div class="imageofme">
+        <Skills  v-bind:skills="skills" v-bind:web_dev="web_dev" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Skills from "./Skills";
 
 export default {
-
   name: "AboutMe",
-    components: {
+  components: {
+    Skills
   },
   props: ["icons"],
   data() {
     return {
-      name:"Mahdi Mohammed",
+      name: "Mahdi Mohammed",
       job_role: "Studying Bsc Computer Science",
-     
+      skills: [
+          {
+              name: "Java",
+              icon: "fab fa-java"
+          }, 
+          {
+              name: "C#",
+              icon: "devicon-csharp-plain colored"
+          },
+          {
+              name: "Python",
+              icon: "fab fa-python"
+          },
+          
+      ],
+      web_dev: [
+          {
+              name: "VueJS",
+              icon: "fab fa-vuejs"
+          }, {
+              name: "NodeJS",
+              icon: "fab fa-node"
+          },
+           {
+              name: "MySQL",
+              icon: "devicon-mysql-plain-wordmark colored"
+          },
+      ]
     };
   }
 };
@@ -35,55 +67,55 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import 'https://use.fontawesome.com/releases/v5.7.1/css/all.css';
-i{
+@import "https://use.fontawesome.com/releases/v5.7.1/css/all.css";
+i {
   padding: 0 0.5rem;
 }
-h1{
+h1 {
   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
   font-weight: 100;
 }
-h2{
-    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-      font-weight: 100;
-
-
+h2 {
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: 100;
 }
 
-.imagecontainer img{
-padding:10px 10px;
+.imagecontainer img {
+  padding: 10px 10px;
 }
+
 * {
   margin: 0;
   padding: 0;
   text-decoration: none;
   color: black;
 }
-.aboutme_wrapper{
+
+.aboutme_wrapper {
   display: grid;
   grid-gap: 30px;
-  max-width:100vw;
-  margin:auto;
-  margin-top:5vh;
+  max-width: 100vw;
+  margin: auto;
+  margin-top: 5vh;
 
-  grid-template-areas: 'information image'
+  grid-template-areas: "information image";
 }
-.information{
-  max-width:30vw;
+.information {
+  max-width: 30vw;
   grid-area: information;
 }
-.imageofme{
-  grid-area:image;
+.imageofme {
+  grid-area: image;
 }
-.imageplaceholder{
-  width:15vw;
-  height: 15vw;
+.imageplaceholder {
+  height:15vw;
+  width: 15vw;
+  background-image: url("../../images/picofme.jpeg");
+  background-size: cover;
   overflow: hidden;
-
-  /**background-image: url('../../images/picofme.jpeg');**/
- background-size: cover;
-    background-repeat: no-repeat;
-    background-position: 50% 50%;  border-radius:15vw;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  border-radius: 15vw;
+  
 }
-
 </style>
---
+++ client/src/components/subcomponents/Skills.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="app">
+      <div class="skills">
+          <h1>Skills</h1><br>
+          <h3>Programming Languages</h3><br><br>
+          <div v-bind:key="skill.id" v-for="skill in skills">
+              <p class = "skill_design"><i :class="skill.icon" /> </p>
+          </div>
+               <h3>Web Development</h3><br><br>
+          <div v-bind:key="web.id" v-for="web in web_dev">
+              <p class = "sss"><i :class="web.icon" /> </p>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  name: "Skills",
+    components: {
+  },
+  props: ["icons","skills"],
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import 'https://use.fontawesome.com/releases/v5.7.1/css/all.css';
+@import 'https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css';
+.app{
+    margin: 0 100px;
+}
+
+.skills h1{
+  text-align: left;
+}
+.skill_design{
+  float: left;
+  padding:10px;
+}
+.skill_design i{
+  font-size:5rem;
+}
+h3{
+  float:left;
+  font-weight: 100;
+}
+</style>
